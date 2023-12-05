8a00cabcbdf78d58810cfcca98012806fd4181de
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-05 09:44:42 -0600

    This has the js changes to the js and css files

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,7 +1,16 @@
 const ListHeader =({ ListName }) => {
+    const signOut = () => {
+      console.log("sign out")
+    }  
+
+
     return (
-      <div >
-        {ListName}
+      <div className="list-header">
+       <h1>       {ListName} </h1>
+      <div className="button-container"> 
+        <button className="create">Add new</button>
+        <button className="signout" onClick={signOut}>Sign out </button>
+      </div>
       </div>
     )
   }
