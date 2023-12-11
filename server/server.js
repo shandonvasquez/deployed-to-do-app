55fcb26ce0b30a9fa086871522d0fc22072be1b6
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-10 22:15:55 -0600

    This has the js changes to the js and css files

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const PORT = process.env.PORT ?? 8000;
+const express = require("express")
+const app = express()
+
+
+app.listen(PORT, () => 'server is running on port ${PORT}')
+
+
+
+
+
+
+
+
+ // on the carpet of servier run with npm run start but you have to be on server with cd server//
