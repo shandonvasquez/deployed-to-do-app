75b294d81d6d2839ddfe8f14d6062c51dcc27c27
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-14 22:52:14 -0600

    This has the js changes to the js and css files

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express")
 const app = express()
-
+const pool = require('./db')    
 //get all todos
 app.get('/todos', async (req, res) => {
     try{
+       const todos = await pool.query('SELECT * FROM todos')
+         res.json(todos.rows)
 //await
     }catch(err){
         console.log(err)
-    
-
     }
 })
 
