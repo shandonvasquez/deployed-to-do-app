31b775417f74d337dd5451eea0dc1457ce3057fd
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-14 22:38:27 -0600

    This has the js changes to the js and css files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const PORT = process.env.PORT ?? 8000;
 const express = require("express")
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 app.listen(PORT, () => console.log (`Server is running on port ${PORT}`))
 
