ad0b076aeeaafca7d19b65bc6448982a9217ac85
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-14 22:49:04 -0600

    This has the js changes to the js and css files

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({ 
-    user: 'postgresql',
-    password: 'Dance4me',
-    host: 'localhost',
-    port: 5434,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
     database: 'todoapp'
 })
 
