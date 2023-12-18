b5767bde1a66dba9a8029f23b82bd63beae956b2
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-17 22:33:33 -0600

    This has the js changes to the js and css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import ListHeader from './components/ListHeader'
 const App =() => {
-  
-  const getData = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
-      const json = await response.json()
-      setTasks(json)
-    } catch (err) {
-      console.error(err)
-    }
-  }
 
-
-
+  // const getData = async () => {
+  //   try {
+  //     const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
+  //     const json = await response.json()
+  //     setTasks(json)
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  // } this part is not working 
 
   return (
     <div className="app" >
