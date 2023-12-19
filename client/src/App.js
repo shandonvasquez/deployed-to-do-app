096c422aa767591d0971b35b43422f72eaecce5f
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-18 22:00:07 -0600

    This has the js changes to the js and css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import ListHeader from './components/ListHeader'
+import { useEffect } from 'react'
+
 const App =() => {
 
-  // const getData = async () => {
-  //   try {
-  //     const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
-  //     const json = await response.json()
-  //     setTasks(json)
-  //   } catch (err) {
-  //     console.error(err)
-  //   }
-  // } this part is not working 
+  const getData = async () => {
+    const userEmail = 'shandon1915211@gmail.com'
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
+      const json = await response.json()
+      //setTasks(json)
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  } 
+
+  useEffect(() =>  getData  , [])
+
 
   return (
     <div className="app" >
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const pool = require('./db')    
 //get all todos
-
+app.use(cors())
 app.get('/todos/:userEmail', async (req, res) => {
-    const { userEmail } = req.params
+    const  userEmail  = req.params
     try {
       const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
       res.json(todos.rows)
