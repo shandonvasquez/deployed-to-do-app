d938453a1a195a7502acf4584445b3f08a283e32
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-21 22:31:31 -0600

    Javascript Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,37 @@
-import ListHeader from './components/ListHeader'
-import ListItem from './components/ListItem'
-import { useEffect, useState } from 'react'
+import ListHeader from './components/ListHeader';
+import ListItem from './components/ListItem';
+import { useEffect, useState } from 'react';
 
-const App =() => {
-  const userEmail = 'shandon1915211@gmail.com'
-    const [ tasks, setTasks ] = useState(null)
-    
+const App = () => {
+  const userEmail = 'shandon1915211@gmail.com';
+  const [tasks, setTasks] = useState([]);
+
   const getData = async () => {
-  
     try {
-      const response = await fetch(`http://localhost:8000/todos${userEmail}`)
-      const json = await response.json()
-      //setTasks(json)
-      console.log(json)
-      setTasks(json)
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      setTasks(json);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  } 
+  };
 
-  useEffect(() =>  getData  , [])
-  console.log(tasks)
+  useEffect(() => {
+    getData();
+  }, []);
 
-//sort by date
-const sortedTask = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
-
-
-
+  // Sort tasks only when tasks have been fetched and are not null
+  const sortedTasks = tasks.length > 0 ? [...tasks].sort((a, b) => new Date(a.date) - new Date(b.date)) : [];
 
   return (
-    <div className="app" >
-      <ListHeader ListName={"Holiday tick list"}/>
-      {sortedTask?.map((task) => <ListItem key={task.id} task={task} />)}
+    <div className="app">
+      <ListHeader ListName={"Holiday tick list"} />
+      {sortedTasks.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
-
-
-
   );
-}
+};
 
 export default App;
+
