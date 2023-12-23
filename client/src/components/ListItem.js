61388d40e1f3148a6a043b28465821ec5ea04bb0
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-22 22:13:46 -0600

    Javascript Changes

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,9 +1,17 @@
 const ListItem =({task}) => {
     return (
-      <div className="list-item" >
-        <p>{task.title}</p>
-      </div>
-    );
+   <li className="list-item">
+    <div className="info-container">
+      <TickIcon/>
+      <p className="task-title">{task.title}</p>
+      <ProgressBar/>
+    </div>
+    <div className="button-container">
+      <button className="edit" >EDIT</button>
+      <button className="delete">DELETE</button>
+    </div>
+
+   </li>
   }
   
   export default ListItem;
