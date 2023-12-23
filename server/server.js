0469246571e37f823fc55e15e7e6c92bda953ba1
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-22 22:12:37 -0600

    Javascript Changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(cors())
 app.get('/todos/:userEmail', async (req, res) => {
     const  {userEmail}  = req.params
     try {
+      
       const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
       res.json(todos.rows)
     } catch (err) {
