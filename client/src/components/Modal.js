5422e7c083fb3616322c2b90e1d2232c59888e45
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-25 16:05:54 -0600

    Javascript Changes

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,24 @@
 const Modal =() => {
-  const mode = 'create';s
+
+  const mode = 'edit';
+
     return (
    <div className="overlay">     
     <div className="modal">
-      <div>
+      <div className="form-tittle-container">
         <h3>Let's {mode} create you task</h3>
+        <button>X</button>
       </div>
+      <form>
+        <input
+        required
+        maxlength={30}
+        plasholder="Your task goes"
+        />
+        <input/>
+        <input type="submit"/>
+
+      </form>
       </div>
    </div>
     );
