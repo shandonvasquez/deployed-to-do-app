48304bd007457921ff79785bb7e1e014d1a404a3
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-26 20:54:41 -0600

    Javascript Changes

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -12,9 +12,14 @@ const Modal = ({ mode, setShowModal, task }) => {
     date: editMode ? '' : new Date(),
   })
 
-  const postData = () =>{
+  const postData = async () =>{
     try{
-      fetch()
+      const response = await fetch('http://localhost:8000/todos', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+      console.log(response)
     } catch(err){
      console.log(err) 
     }
