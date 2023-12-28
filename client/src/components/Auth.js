4cd0c6b8cca2e35501e50b28a8c2cdc178040ae8
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-27 22:11:39 -0600

    React changes

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,13 +1,15 @@
-
 const Auth =() => {
   const isLogin = false;
     return (
       <div className="auth-container">
-        <form>
+       <div className="auth-container-box">
+       <form>
+        <h2>{isLogin ? 'Please log in' : 'Please sing up!'}</h2>
           <input></input>
           <input></input>
           <input></input>
         </form>
+       </div>
       </div>
     );
   }
