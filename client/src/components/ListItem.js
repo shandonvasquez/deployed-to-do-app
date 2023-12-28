8f21e52c3195efefdd7124e37cd6897dec47bf3e
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-27 21:58:43 -0600

    React changes

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -6,6 +6,20 @@ import ProgressBar from './ProgressBar';
 
 const ListItem =({task, getData}) => {
   const [showModal, setShowModal] = useState(false);
+
+const deleteItem = async () => {
+  try{
+    await fetch(`http://localhost:8000/todos/${task.id}`,{
+      if (response.status === 200){
+        console.log('success')
+        getData()
+    }
+    } catch(err){
+      console.log(err)
+    }
+  }
+    
+
     return (
    <li className="list-item">
 
@@ -16,7 +30,7 @@ const ListItem =({task, getData}) => {
     </div>
     <div className="button-container">
       <button className="edit" onClick={() => setShowModal(true)} >EDIT</button>
-      <button className="delete">DELETE</button>
+      <button className="delete" onClick={deleteItem}>DELETE</button>
     </div>
       {showModal && <Modal mode={'edit'} setShowModal={setShowModal} getData={getData} task={task}/>}
    </li>
