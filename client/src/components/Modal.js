252f82d752bcea01284209eee19ee26ae4120188
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-27 22:06:01 -0600

    React changes

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -33,7 +33,7 @@ const Modal = ({ mode, setShowModal, getData, task }) => {
   const editData = async (e) =>{
     e.preventDefault()  
     try{
-        const response = await fetch(`http://localhost:8000/todos/${task.id}`,{
+        const response = await fetch(`${process.env.REACT_APP_SERVERURL}{task.id}`,{
         method: 'PUT',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
