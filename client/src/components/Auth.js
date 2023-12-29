19f1072d50b986155a7e5b3f335e4dab042155f4
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-12-28 21:36:48 -0600

    React changes

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -5,7 +5,7 @@ const Auth =() => {
        <div className="auth-container-box">
        <form>
         <h2>{isLogin ? 'Please log in' : 'Please sing up!'}</h2>
-          <input type="email"></input>
+          <input type="email" placeholder="email"></input>
           <input type="password"></input>
           <input type="password" ></input>
         </form>
