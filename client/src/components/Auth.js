acad0109eb80480dd24d483364f4ed8c7eb16ca4
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-02 21:36:59 -0600

    Javascript Changes

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,38 +1,83 @@
-import {useState} from 'react';
-const Auth =() => {
-  const [error, setError] = useState(null);
+import { useState } from 'react'
+import { useCookies} from 'react-cookie'
+
+const Auth = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const [isLogIn, setIsLogin] = useState(true)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [error, setError] = useState(null)
 
   const viewLogin = (status) => {
-   setError(null);
-   setLogin(status);
+    setError(null)
+    setIsLogin(status)
+  }
+
+  const handleSubmit = async (e, endpoint) => {
+    e.preventDefault()
+    if (!isLogIn && password !== confirmPassword) {
+      setError('Make sure passwords match!')
+      return
+    }
+    
+    const response = await fetch(`${process.env.REACT_APP_SERVERURL}/${endpoint}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({email, password})
+    })
+
+    const data = await response.json()
+
+    if (data.detail) {
+      setError(data.detail)
+    } else {
+      setCookie('Email', data.email)
+      setCookie('AuthToken', data.token)
+
+      window.location.reload()
+    }
+
   }
 
 
-    return (
-      
-      <div className="auth-container">
-       <div className="auth-container-box">
-       <form>
-        <h2>{isLogin ? 'Please log in' : 'Please sing up!'}</h2>
-          <input type="email" placeholder="email"></input>
-          <input type="password" placeholder="password"></input>
-          {!isLogin &&           <input type="password" placeholder= "confirm password" ></input>} 
-        <input type="submit" className="create"/>
-        {error && <p className="error">{error}</p>} 
-        <p></p>
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogIn  ? 'Please log in' : 'Please sign up!'}</h2>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isLogIn && <input
+            type="password"
+            placeholder="confirm password"
+            onChange={(e) =>setConfirmPassword(e.target.value)}
+          />}
+          <input type="submit" className="create" onClick={(e) => handleSubmit(e, isLogIn ? 'login' : 'signup')} />
+          {error && <p>{error}</p>}
         </form>
-        <div className='auth-options'>
-          <button onClick={() => viewLogin(false)} >
-            Sing Up</button>
-            style={{backgroundColor: !isLogIn}}
-          <button onClick={() => viewLogin(true)}>
-            Login</button>
+        <div className="auth-options">
+          <button
+            onClick={() => viewLogin(false)}
+            style={{backgroundColor : !isLogIn ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)'}}
+          >Sign Up</button>
+          <button
+            onClick={() => viewLogin(true)}
+            style={{backgroundColor : isLogIn ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)'}}
+          >Login</button>
         </div>
 
-       </div>
       </div>
-    );
-  }
-  
-  export default Auth;
-  
+    </div>
+  )
+}
+
+export default Auth
