771f6f809c61b7d94a1d61112633a5bf3601ad8a
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-03 20:55:35 -0600

    Javascript Changes

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,23 +1,29 @@
-import Modal from "./Modal.js";
-import { useState } from "react";
+import Modal from './Modal'
+import { useState } from 'react'
+import { useCookies } from 'react-cookie'
 
-const ListHeader =({ ListName, getData }) => {
-    const [showModal, setShowModal] = useState(false);
-    const signOut = () => {
-      console.log("sign out")
-    }  
+const ListHeader = ({ listName, getData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const [showModal, setShowModal] = useState(false)
 
+  const signOut = () => {
+    console.log('signout')
+    removeCookie('Email')
+    removeCookie('AuthToken')
+    window.location.reload()
 
-    return (
-      <div className="list-header">
-       <h1>       {ListName} </h1>
-      <div className="button-container"> 
-        <button className="create" onClick={() => setShowModal(true)}>Add new</button>
-        <button className="signout" onClick={signOut}>Sign out </button>
-      </div>
-        {      <Modal mode={'create'} setShowModal = {setShowModal} getData={getData}/>}
-      </div>
-    )
   }
-  
-  export default ListHeader;
+
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
+      </div>
+      {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData} />}
+    </div>
+  )
+}
+
+export default ListHeader
